2ae4a51775baff0ef3359298d33a6efe24b992e7
denniztr <denniztr@gmail.com>
2024-04-08 14:43:54 +0300

    removing boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './global/styles.scss';
+
+function App() {
+  return (
+    <div className="scss">
+      Hello main page!
+      <div className='scss_another'>
+        another div
+      </div>
+    </div>
+  );
+}
+
+export default App;
